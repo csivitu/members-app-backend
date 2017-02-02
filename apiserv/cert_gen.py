7f3930f747ebfe8302dde7a378d1b4712af4d509
palashgo <palash.golecha2016@vitstudent.ac.in>
2017-02-02 17:23:59 +0530

    Added signed certificate

--- apiserv/cert_gen.py
+++ apiserv/cert_gen.py
@@ -1,23 +1,33 @@
-"""Generates certificates for the user
-"""
-
+from PIL import Image,ImageFont,ImageDraw
+import os
 from django.conf import settings
-from PIL import Image, ImageFont, ImageDraw
 
+def get_school(reg):
+	code=reg[2:5]
+	if(code in ['BCE','BCS','MIS','BCB']):
+		return 'SCOPE'
+	elif(code in ['BPI','BEM','BME']):
+		return 'SMEC'
+	elif(code in ['BIT','PHD']):
+		return 'SITE'
+	elif(code in ['BEC']):
+		return 'SENSE'
+	elif(code in ['BEI','BEE']):
+		return 'SELECT'
+	elif(code in ['BCM']):
+		return 'SCALE'
+	else:
+		return ""
+def cert_gen(name,regno,year):
+	school=get_school(regno)
+	year=str(year)[2:4]+" "+str(year+1)[2:4]
+	img = Image.open(settings.BASE_DIR+"/cert/base.jpg")
+	draw = ImageDraw.Draw(img)
+	font = ImageFont.truetype(settings.BASE_DIR+"/cert/font.ttf", 84)
+	font_SM = ImageFont.truetype(settings.BASE_DIR+"/cert/font.ttf", 64)
+	draw.text((1400, 905),name,(0,0,0),font=font)
+	draw.text((2800, 930),regno,(0,0,0),font=font_SM)
+	draw.text((500, 1040),of,(0,0,0),font=font)
+	draw.text((3028, 1060),year,(0,0,0),font=font_SM)
+	return img
 
-def cert_gen(name, reg_no, year, of=""):
-    year = str(year)[2:4] + " " + str(year + 1)[2:4]
-    img = Image.open(settings.BASE_DIR + "/cert/base.jpg")
-
-    # Load fonts
-    font = ImageFont.truetype(settings.BASE_DIR + "/cert/font.ttf", 84)
-    font_small = ImageFont.truetype(settings.BASE_DIR + "/cert/font.ttf", 64)
-
-    # Write information
-    draw = ImageDraw.Draw(img)
-    draw.text((1400, 960), name, (0, 0, 0), font=font)
-    draw.text((2800, 980), reg_no, (0, 0, 0), font=font_small)
-    draw.text((500, 1100), of, (0, 0, 0), font=font)
-    draw.text((3040, 1115), year, (0, 0, 0), font=font_small)
-
-    return img
