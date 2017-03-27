5b6b9f9e355f43de758593bc7e3d1c7f356c4d70
Palash <palash.golecha2016@vitstudent.ac.in>
2017-03-27 21:49:13 +0530

    Firebase integration

--- apiserv/models.py
+++ apiserv/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils import timezone
-
+from pyfcm import FCMNotification
+from django.conf import settings
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
@@ -16,7 +17,7 @@ class Event(models.Model):
     date = models.DateTimeField(default=timezone.now)
     time = models.CharField(max_length=50)
     link = models.CharField(max_length=150)
-
+    short_desc = models.CharField(max_length=80)
     def __str__(self):
         return self.name
 
@@ -29,11 +30,12 @@ class User(AbstractUser):
 
 
 '''
+Untested Code
+'''
 @receiver(post_save, sender=Event)
 def send_notification(sender, instance, created, **kwargs):
+    push_service = FCMNotification(api_key=settings.FIREBASE_KEY)
+    message_title = "Upcoming Event - CSI"
+    message_body = instance.short_desc
+    push_service.notify_topic_subscribers(topic_name="newevents", message_body=message_body)
 
-<FIREBASE INTEGRATION>
-'''
-
-
-# Create your models here.
